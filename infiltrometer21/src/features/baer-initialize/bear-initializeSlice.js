169c658822cf36464dc174b03c523f546e5967b0
August Miller <augustamiller77@gmail.com>
2021-09-17 14:57:45 -0400

    Added react-compound-timer and reports slice

--- infiltrometer21/src/features/baer-initialize/bear-initializeSlice.js
+++ infiltrometer21/src/features/baer-initialize/bear-initializeSlice.js
@@ -1,12 +1,14 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 
+
 const initialState = {};
 
 export const baerInitializeSlice = createSlice({
     name: 'baerInitialize',
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
-  reducers: {}
+  reducers: {
+  }
 });
 export default baerInitializeSlice.reducer;
--- infiltrometer21/src/features/baer-replication/bear-replicationSlice.js
+++ infiltrometer21/src/features/baer-replication/bear-replicationSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
+import {TimerStateValues} from 'react-compound-timer'
 const initialState = {};
 
 export const baerReplicationSlice = createSlice({
