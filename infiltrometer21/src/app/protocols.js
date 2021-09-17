6c5ded2e432d31956e11d532d2bdb097882a9b19
August Miller <augustamiller77@gmail.com>
2021-09-17 10:15:56 -0400

    Added react-router-dom

---
+++ infiltrometer21/src/app/protocols.js
@@ -0,0 +1,6 @@
+//Contains the reference to all protocol names
+export const protocols = {
+    Baer: "BAER_PROTOCOL",
+    Standard: "TIME_PROTOCOL",
+    Experimental: "VOLUME_PROTOCOL "
+}
---
+++ infiltrometer21/src/app/soilTypes.js
@@ -0,0 +1,6 @@
+//contains the single reference to the soil types to be used in reports under key "soilType"
+export const soilTypes = {
+    clay: "CLAY",
+    clayLoam: "CLAY_LOAM",
+    loam: "LOAM"
+}
