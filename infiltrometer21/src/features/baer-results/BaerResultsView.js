beb9f86e2e3e6058e37b595d051099df1bc677f6
August Miller <augustamiller77@gmail.com>
2021-09-18 06:52:52 -0400

    Added updated results view

--- infiltrometer21/src/features/baer-results/BaerResultsView.js
+++ infiltrometer21/src/features/baer-results/BaerResultsView.js
@@ -1,11 +1,8 @@
 //The Page we are displaying for the baer Initialize view
 import React from 'react';
 
-<<<<<<< HEAD
+
 const  BaerResultsView = ()=> {
-=======
-function BaerResultsView() {
->>>>>>> 78bb0b272eb4a705b282305de49fe4500958c841
   return (<div>Bear Replication View</div>);
 }
 export default BaerResultsView;
