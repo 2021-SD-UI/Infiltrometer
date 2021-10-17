4c784de4afc9c19685f655c76a628c2a0581e698
August Miller <augustamiller77@gmail.com>
2021-10-17 14:27:52 -0400

    Updated Documentation

--- infiltrometer21/src/App.js
+++ infiltrometer21/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import BaerInitializeView from './features/baer/baer-initialize/BaerInitializeView';
 import BaerReplicationView from './features/baer/baer-replication/BaerReplicationView';
 import BaerResultsView from './features/baer/baer-results/BaerResultsView';
-import { ShowReportsButton } from './features/reports/ShowReportsButon';
 import { RegressionTesting } from './features/regression/RegressionTesting';
 import { Redirector } from './features/page-redirection/Redirector';
 import { NavBar } from './features/navBar/NavBar';
