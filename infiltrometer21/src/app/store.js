18c85c841cec824cd81c55904bec652ba9b635fc
bnostrom <bnostrom@mtu.edu>
2021-10-22 18:06:08 -0400

    Redux-Persist Added
    store now persists

--- infiltrometer21/src/app/store.js
+++ infiltrometer21/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
+import { combineReducers }  from 'redux';
 import counterReducer from '../features/counter/counterSlice';
 import baerInitializeReducer from '../features/baer/baer-initialize/bear-initializeSlice';
 import baerReplicationReducer from '../features/baer/baer-replication/bear-replicationSlice';
@@ -7,18 +7,32 @@ import baerResultsReducer from '../features/baer/baer-results/bear-resultsSlice'
 import reportsSlice from '../features/reports/reportsSlice';
 import {reducer as reduxFormReducer} from 'redux-form'
 import  redirectReducer  from '../features/page-redirection/redirector-slice';
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({
+  baerInitialize: baerInitializeReducer,
+  baerReplication: baerReplicationReducer,
+  baerResults: baerResultsReducer,
+  counter: counterReducer,
+  reports: reportsSlice,
+  form: reduxFormReducer,
+  redirector: redirectReducer
+
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig,reducers);
+
+
 
 
 
 
 export const store = configureStore({
-  reducer: {
-    baerInitialize: baerInitializeReducer,
-    baerReplication: baerReplicationReducer,
-    baerResults: baerResultsReducer,
-    counter: counterReducer,
-    reports: reportsSlice,
-    form: reduxFormReducer,
-    redirector: redirectReducer
-  },
+    reducer: persistedReducer
 });
