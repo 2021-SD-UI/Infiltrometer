56ae7adb930c04b4c97d386ddd50ba7b30e91f20
August Miller <augustamiller77@gmail.com>
2021-10-23 16:34:28 -0400

    Added the reportsDataPackager that has data for processesing reports to rate, average rate and severity

--- infiltrometer21/src/features/baer/baer-results/table.js
+++ infiltrometer21/src/features/baer/baer-results/table.js
@@ -2,7 +2,7 @@ import React, {Component, useEffect, useState} from 'react'
 import {useSelector} from "react-redux";
 import {selectCurId, selectReports} from "../../reports/reportsSlice";
 import {SeverityRatings} from "../../reports/severityRatings";
-
+import { findAverageRate, findSeverityRating, findRate } from '../../reports/reportsDataPackager';
 const Table =()=> {
     const rawReports = useSelector(selectReports);
 
@@ -11,51 +11,7 @@ const Table =()=> {
 
     const [state, setState] = useState(filterReadings());
 
-    /**
-     * Find ml/min for a reading
-     * @param {The current reading index} i
-     */
-    function findRate(i){
-        if(i>0){
 
-            let deltaV = curReport.readings[i-1].volume - curReport.readings[i].volume;
-            let deltaT = curReport.readings[i].secondsElapsed - curReport.readings[i-1].secondsElapsed;
-            return (deltaV)/(deltaT/60);
-        }
-        else{
-            return 0;
-        }
-    }
-
-    /**
-     * Find average rate (ml/min)
-     * Relies on findRate(i) to get rates for calculating the average
-     * @param {The current reading index} i
-     */
-    function findAverageRate() {
-        let sum = 0;
-
-        for(let i = 0; i < curReport.readings.length; i++) {
-            sum += findRate(i);
-        }
-
-        return sum/(curReport.readings.length - 1);
-    }
-
-    /**
-     * Returns severity rating based on average rate
-     * See severityRatings.js for severity rating values
-     * @param {Average flow rate} avgRate
-     */
-    function findSeverityRating(avgRate) {
-        if (avgRate >= SeverityRatings.None.min)
-            return <td className="text-center">None</td>
-        if (avgRate >= SeverityRatings.Strong.min && avgRate < SeverityRatings.Strong.max)
-            return <td className="text-center">Strong</td>
-        if (avgRate >= SeverityRatings.Weak.min && avgRate < SeverityRatings.Weak.max)
-            return <td className="text-center">Weak</td>
-        else return <td className="text-center">N/A</td>
-    }
 
     /**
      * Create an array to use for a table row from reading data
@@ -72,7 +28,7 @@ const Table =()=> {
                 id: i,
                 Time: curReport.readings[i].secondsElapsed,
                 Volume: curReport.readings[i].volume,
-                Rate: findRate(i)
+                Rate: findRate(i, curReport)
 
             };
         }
@@ -143,8 +99,8 @@ const Table =()=> {
                             <th className="text-center">SEVERITY RATING</th>
                         </tr>
                         <tr class="table-striped">
-                            <td className="text-center">{findAverageRate().toPrecision(4)}</td>
-                            {findSeverityRating(findAverageRate())}
+                            <td className="text-center">{findAverageRate(curReport).toPrecision(4)}</td>
+                            {findSeverityRating(findAverageRate(curReport))}
                         </tr>
                     </tbody>
                 </table>
---
+++ infiltrometer21/src/features/reports/reportsDataPackager.js
@@ -0,0 +1,48 @@
+    
+    
+    import { SeverityRatings } from "./severityRatings";
+    /**
+     * Find ml/min for a reading
+     * @param {The current reading index} i
+     */
+    export function findRate(readingIndex, report){
+        if(readingIndex>0){
+
+            let deltaV = report.readings[readingIndex-1].volume - report.readings[readingIndex].volume;
+            let deltaT = report.readings[readingIndex].secondsElapsed - report.readings[readingIndex-1].secondsElapsed;
+            return (deltaV)/(deltaT/60);
+        }
+        else{
+            return 0;
+        }
+    }
+
+    /**
+     * Find average rate (ml/min)
+     * Relies on findRate(i) to get rates for calculating the average
+     * @param {The current reading index} i
+     */
+    export function findAverageRate(report) {
+        let sum = 0;
+
+        for(let i = 0; i < report.readings.length; i++) {
+            sum += findRate(i, report);
+        }
+
+        return sum/(report.readings.length - 1);
+    }
+
+    /**
+     * Returns severity rating based on average rate
+     * See severityRatings.js for severity rating values
+     * @param {Average flow rate} avgRate
+     */
+    export function findSeverityRating(avgRate) {
+        if (avgRate >= SeverityRatings.None.min)
+            return <td className="text-center">None</td>
+        if (avgRate >= SeverityRatings.Strong.min && avgRate < SeverityRatings.Strong.max)
+            return <td className="text-center">Strong</td>
+        if (avgRate >= SeverityRatings.Weak.min && avgRate < SeverityRatings.Weak.max)
+            return <td className="text-center">Weak</td>
+        else return <td className="text-center">N/A</td>
+    }
