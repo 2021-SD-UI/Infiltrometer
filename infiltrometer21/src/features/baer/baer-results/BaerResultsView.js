5a5c3d6fe9923590d861a481fd9fe632e6ea9088
Harry Taylor <35974116+hdudeness@users.noreply.github.com>
2021-11-11 17:20:10 -0500

    Converted BAER results view to react-bootstrap

--- infiltrometer21/src/features/baer/baer-results/BaerResultsView.js
+++ infiltrometer21/src/features/baer/baer-results/BaerResultsView.js
@@ -9,6 +9,7 @@ import { CSVLink } from "react-csv";
 import { makeCSV } from "../../reports/reportsDataPackager";
 import TextareaAutosize from 'react-textarea-autosize';
 import { selectNotes, setNotes } from "../../reports/reportsSlice";
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 const BaerResultsView = () => {
 
@@ -23,76 +24,58 @@ const BaerResultsView = () => {
     setState(event);
   }
   useEffect(() => setState(notes), []);
-  return (<div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-2"></div>
-      <div class="col-sm-8 text-center">
-        <div class="display-4">Results</div>
-
-      </div>
-      <div class="col-sm-2"></div>
-
-    </div>
-    <div class="row">
-      <div class="col-sm-2"></div>
-      <div class="col-sm-8">
-        <div class="container">
-          <Table></Table>
+  
+  return (
+      <Container className="mt-3">
+        <div class="rounded border shadow">
+          <h1 className="pt-5 display-4">Results</h1>
+          <Row className="mt-4">
+            <Col>
+              <Table>{/* This table is rendered from table.js */}</Table>
+            </Col>
+          </Row>
+          <Row className="justify-content-center mb-5">
+            <Col xs={11}>
+              <h1 className="display-6">Add Notes to Report</h1>
+              <TextareaAutosize className="w-100" value={state} onChange={(e) => changeNotes(e.target.value)}/>
+            </Col>
+          </Row>
+          <Row className="my-2 text-center">
+            <Col>
+              <Button
+                variant="dark"
+                className="w-50"
+                size="lg"
+                onClick={() => dispatch(setPage("/Infiltrometer/baer-initialize"))}
+              >
+                New Test
+              </Button>
+            </Col>
+          </Row>
+          <Row className="text-center">
+            <Col>
+              <Button
+                variant="secondary"
+                className="w-50"
+                size="lg"
+                onClick={() => dispatch(setPage("/Infiltrometer/reports"))}
+              >
+                Reports
+              </Button>
+            </Col>
+          </Row>
+          <Row className="mt-2 mb-5 text-center">
+            <Col>
+              <CSVLink 
+                {/*onClick ->*/ ...makeCSV(curReport)} 
+                class="btn btn-success btn-lg w-50"
+              >
+                Download Report
+              </CSVLink>
+            </Col>
+          </Row>
         </div>
-      </div>
-      <div class="col-sm-2"></div>
-    </div>
-
-    <div class="row">
-
-      <div class="col-sm-2"></div>
-      <div class="col-sm-8 text-center mb-2">
-
-        <div class="container">
-          <div class="row">
-            <div class="col-2"></div>
-            <div class="col-8 text-center">
-              <div class="display-6">Notes</div>
-
-            </div>
-            <div class="col-2"></div>
-          </div>
-
-
-          <TextareaAutosize className="row" class="w-100" value={state} onChange={(e) => changeNotes(e.target.value)} />
-        </div>
-      </div>
-      <div class="col-sm-2"></div>
-
-    </div>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-2"></div>
-        <div class="col text-center">
-          <div class="btn btn-dark w-50" onClick={
-            () => dispatch(setPage("/Infiltrometer/baer-initialize"))
-          }>
-            New Test
-          </div>
-          <div class="row mt-2"></div>
-
-
-          <div class="btn btn-secondary w-50" onClick={
-            () => dispatch(setPage("/Infiltrometer/reports"))
-          }>
-            Reports
-          </div>
-          <div class="row mt-4"></div>
-          <CSVLink {...makeCSV(curReport)} class="btn btn-success w-25">
-            Download
-          </CSVLink>
-
-        </div>
-        <div class="col-2"></div>
-      </div>
-    </div>
-
-  </div>);
+      </Container>
+  );
 }
 export default BaerResultsView;
