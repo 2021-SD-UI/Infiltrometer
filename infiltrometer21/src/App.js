faeab97067790db159237239038403d922154234
August Miller <augustamiller77@gmail.com>
2021-11-30 11:57:53 -0500

    Service worker registered and hashrouter added

--- infiltrometer21/src/App.js
+++ infiltrometer21/src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
 import BaerInitializeView from './features/baer/baer-initialize/BaerInitializeView';
 import BaerReplicationView from './features/baer/baer-replication/BaerReplicationView';
 import BaerResultsView from './features/baer/baer-results/BaerResultsView';
 import { RegressionTesting } from './features/regression/RegressionTesting';
 import { Redirector } from './features/page-redirection/Redirector';
 import { NavBar } from './features/navBar/NavBar';
-import SinglePagePDFView from "./features/pdfviewer/pdf-viewer";
 import ReportsTable from './features/reports/ReportsTable';
 import { MiniDiskManualView, FieldGuideManualView } from './features/pdfviewer/Manuals';
 import { HomePage } from './features/homepage/HomePage';
 const App = () => {
   return (
 
-    <BrowserRouter>
+    <HashRouter>
       <title>Infiltrometer Companion</title>
       <NavBar />
       <Redirector />
@@ -48,7 +47,7 @@ const App = () => {
 
       </Switch>
 
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- infiltrometer21/src/features/pdfviewer/pdf-viewer.js
+++ infiltrometer21/src/features/pdfviewer/pdf-viewer.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-import {Document, Page, pdfjs} from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function SinglePagePDFView(props) {
-    
-    let {pdf, reload} = props
-    
+
+    let { pdf, reload } = props
+
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
     const [value, setValue] = useState(1);
     const [finalValue, setFinalValue] = useState(null);
 
-    function onDocumentLoadSuccess({numPages}) {
-        
-        if (reload){
-           
+    function onDocumentLoadSuccess({ numPages }) {
+
+        if (reload) {
+
             previousPage(); nextPage(); reload = false;
         }
         setNumPages(numPages);
         setPageNumber(1);
     }
-    
+
     function changePage(offset) {
         setPageNumber(prevPageNumber => prevPageNumber + offset);
     }
@@ -32,64 +32,64 @@ export default function SinglePagePDFView(props) {
     function nextPage() {
         changePage(1);
     }
-   
+
     return (
-    <>
-        <Container>
-            <div class="rounded border shadow">
-                <h1 className="pt-2 display-4">Manuals</h1>
-                <Row className="p-3 text-center">
-                    <Col></Col>
-                    <Col>
-                        <Document
-                            class="text-center"
-                            file={pdf}
-                            options = {{workerSrc :"/pdf.worker.js"}}
-                            onLoadSuccess = {onDocumentLoadSuccess}
-                        >
-                        <Page pageNumber= {pageNumber}/>
-                    
-                        </Document>
-                        <Form>
-                            <Form.Group>
-                                <Form.Control 
-                                    type="range"
-                                    disabled
-                                    min="1" 
-                                    max={numPages} 
-                                    value={pageNumber}
-                                    className="w-100" 
-                                />
-                            </Form.Group>
-                            <Form.Label>Page {pageNumber} of {numPages}</Form.Label>
-                        </Form>
-                    </Col>
-                    <Col></Col>
-                </Row>
-                <Row className="pb-3 text-center">
-                    <Col>
-                        <Button 
-                            variant="secondary" 
-                            className="w-50" 
-                            disabled={pageNumber<=1} 
-                            onClick={previousPage}
-                        >
-                            Previous
-                        </Button>
-                    </Col>
-                    <Col>
-                        <Button 
-                            variant="dark" 
-                            className="w-50" 
-                            disabled={pageNumber>=numPages} 
-                            onClick={nextPage}
-                        >
-                            Next
-                        </Button>
-                    </Col>
-                </Row>
-            </div>
-        </Container>
-    </>
+        <>
+            <Container>
+                <div class="rounded border shadow">
+                    <h1 className="pt-2 display-4">Manuals</h1>
+                    <Row className="p-3 text-center">
+                        <Col></Col>
+                        <Col>
+                            <Document
+                                class="text-center"
+                                file={pdf}
+
+                                onLoadSuccess={onDocumentLoadSuccess}
+                            >
+                                <Page pageNumber={pageNumber} />
+
+                            </Document>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Control
+                                        type="range"
+                                        disabled
+                                        min="1"
+                                        max={numPages}
+                                        value={pageNumber}
+                                        className="w-100"
+                                    />
+                                </Form.Group>
+                                <Form.Label>Page {pageNumber} of {numPages}</Form.Label>
+                            </Form>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                    <Row className="pb-3 text-center">
+                        <Col>
+                            <Button
+                                variant="secondary"
+                                className="w-50"
+                                disabled={pageNumber <= 1}
+                                onClick={previousPage}
+                            >
+                                Previous
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Button
+                                variant="dark"
+                                className="w-50"
+                                disabled={pageNumber >= numPages}
+                                onClick={nextPage}
+                            >
+                                Next
+                            </Button>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+        </>
     );
 }
