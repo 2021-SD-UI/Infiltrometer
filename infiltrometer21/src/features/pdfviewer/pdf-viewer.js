2ce905017392393d5423c1a64bbf17db4cd5db00
August Miller <augustamiller77@gmail.com>
2021-12-04 16:38:28 -0500

    Update pdf-viewer.js

--- infiltrometer21/src/features/pdfviewer/pdf-viewer.js
+++ infiltrometer21/src/features/pdfviewer/pdf-viewer.js
@@ -1,32 +1,9 @@
 
 import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import { Container, Col, Form, Row, Button } from 'react-bootstrap';
-import FieldGuide from './FieldGuide.pdf'
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
-
-
+import { Container } from 'react-bootstrap';
 
 export const PdfViewer = ({ pdf }) => {
-    pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-    const [pageNumber, setPageNumber] = useState(1);
-    const [numPages, setNumPages] = useState(1);
     //  const pageNumber = pdfjs.getNumPages(pdf);
-
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-        setPageNumber(1);
-    }
-    function changePage(offset) {
-        setPageNumber(prevPageNumber => prevPageNumber + offset);
-    }
-    function previousPage() {
-        changePage(-1);
-    }
-    function nextPage() {
-        changePage(1);
-    }
-
     return (
 
         <>
