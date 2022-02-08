05b8140e730a8e0433498974b1e4c2948786d3af
Harry Taylor <35974116+hdudeness@users.noreply.github.com>
2022-02-07 22:15:15 -0500

    Finished conductivity graph

--- infiltrometer21/src/features/standard/standard-results/ConductivityGraph.js
+++ infiltrometer21/src/features/standard/standard-results/ConductivityGraph.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { soilTypes } from "../../../app/soilTypes";
 import { Protocols } from "../../reports/protocols";
 import { selectCurId, selectReports } from "../../reports/reportsSlice";
+import { selectInitialVolume, selectInfiltrometerRadius } from '../../baer/baer-initialize/bear-initializeSlice';
 import LineChart from 'react-linechart';
 import { methods } from "../../regression/regression-js";
 import '../../../../node_modules/react-linechart/dist/styles.css';
@@ -11,36 +12,20 @@ import { Container } from "react-bootstrap";
 const ConductivityGraph = () => {
 
     const reports = useSelector(selectReports);
-
-    //Use this report to display graph data
     const curReport = reports[useSelector(selectCurId)];
-
-
-
-    //checks if the values in the cure report are valid for creating a conductivity graph
-    const validValues = () => {
-        //don't render if the following are true
-        if (curReport.protocol !== Protocols.Standard) return false;
-        if (curReport.infiltrometerData.radius == undefined || curReport.infiltrometerData.radius < 0) return false;
-        if (curReport.infiltrometerData.soilType == soilTypes.undefined) return false;
-
-
-        //show graph here, data is valid
-        return true;
-    }
-
+    const initialVolume = Number(useSelector(selectInitialVolume));
+    const radius = curReport.infiltrometerData.infiltrometerRadius;
 
     // Will give you the format:
     // [{x,y}, {x,y}, ... ]
     // to be used in regression
     function readingsArray() {
         let points = [];
+
         curReport.readings.forEach(r => {
-            let point = { x: Number(r.secondsElapsed), y: Number(r.volume) };
+            let point = { x: Math.sqrt(Number(r.secondsElapsed)), y: ((initialVolume - Number(r.volume)) / (Math.PI * Math.pow(radius, 2))) };
             points.push(point);
-            //methods.polynomial(points, {order: 2, precision: 15});
         });
-        // [{ x: 1, y: 2 }, { x: 3, y: 5 }, { x: 7, y: -3 }]
         return points;
     }
 
@@ -51,18 +36,46 @@ const ConductivityGraph = () => {
         }
     ];
 
-    return (
-        <>
-            <Container className="text-center">
+    // The graph must be given data, even if its empty.
+    const noData = [
+        {
+            color: "steelblue",
+            points: [ {x: 0, y: 0} ]
+        }
+    ];
+
+    // Calculations require radius, so if radius is
+    // null then don't render the graph.
+    const Graph = () => {
+        if (!radius) {
+            return (
                 <LineChart
                     width={380}
                     height={400}
-                    xLabel="Time (s)"
-                    yLabel="Conductivity"
+                    xLabel="No data to show"
+                    yLabel=""
+                    data={noData}
+                />
+            );
+        }
+        else {
+            return (
+                <LineChart
+                    width={380}
+                    height={400}
+                    xLabel="Square Root of Time (s)"
+                    yLabel="Conductivity (cm)"
                     data={data}
                 />
+            );
+        }
+    }
+
+    return (
+        <>
+            <Container className="text-center my-4">
+                <Graph/>
             </Container>
-            {validValues ? "The values are valid" : "The values are not valid!"}
         </>
     );
 }
