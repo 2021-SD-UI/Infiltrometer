76ccde23f7a9e2e23334506f649af49d0c8765ad
Harry Taylor <35974116+hdudeness@users.noreply.github.com>
2022-04-06 18:14:47 -0400

    Cleaned up Standard results view

--- infiltrometer21/src/features/standard/standard-results/StandardResultsView.js
+++ infiltrometer21/src/features/standard/standard-results/StandardResultsView.js
@@ -1,5 +1,5 @@
 //The Page we are displaying for the baer Initialize view
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row, Alert } from 'react-bootstrap';
 import Table from '../../baer/baer-results/table';
 import { Protocols } from '../../reports/protocols';
@@ -10,29 +10,28 @@ import ConductivityGraph from './ConductivityGraph';
 import { ResultsViewPhotos } from '../../reused-components/results-views/ResultsViewPhotos';
 
 const StandardResultsView = () => {
+  const [show, setShow] = useState(true);
 
   return (
-    <Container className="mt-5">
-      <div class="rounded border shadow">
-        <h1 className="mt-5 display-4">Results</h1>
-        <Row className="mt-5">
+    <Container className="mt-4 p-3 rounded border shadow">
+      <h1 className="mb-4 display-4">Results</h1>
+      <Row>
+        <Col>
+          <Table protocol={Protocols.Standard} editable>{/* This table is rendered from table.js */}</Table>
+        </Col>
+      </Row>
+      <Container>
+        <Row className="justify-content-center">
           <Col>
-            <Table protocol={Protocols.Standard} editable>{/* This table is rendered from table.js */}</Table>
+            <Alert show={show} onClose={() => setShow(false)} variant="info" dismissible className="text-center">Need to change volume data? Tap corrosponding table entries to edit.</Alert>
           </Col>
         </Row>
-        <Container>
-          <Row className="justify-content-center">
-            <Col>
-              <Alert variant="info" className="text-center">Need to change volume data? Tap corresponding table entries to edit.</Alert>
-            </Col>
-          </Row>
-        </Container>
-        <ConductivityForm />
-        <ConductivityGraph />
-        <ResultsViewNotes />
-        <ResultsViewPhotos />
-        <ResultsViewButtons protocol={Protocols.Standard} />
-      </div>
+      </Container>
+      <ConductivityForm />
+      <ConductivityGraph />
+      <ResultsViewNotes />
+      <ResultsViewPhotos />
+      <ResultsViewButtons protocol={Protocols.Standard} />
     </Container>
   );
 }
