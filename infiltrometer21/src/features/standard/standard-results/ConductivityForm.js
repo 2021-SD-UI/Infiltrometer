cd1674a6b6033fe0a7c37b6c45255b2c6b650747
Harry Taylor <35974116+hdudeness@users.noreply.github.com>
2022-04-06 18:42:20 -0400

    Fixed conductivity form inputs

--- infiltrometer21/src/features/standard/standard-results/ConductivityForm.js
+++ infiltrometer21/src/features/standard/standard-results/ConductivityForm.js
@@ -53,96 +53,95 @@ const ConductivityForm = () => {
 
     return (
         <>
-            <Container>
-                <div class="rounded border shadow">
-                    <Form className="p-5" noValidate validated onSubmit={handleSubmit}>
-                        <Row>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label className="pt-3 display-6">Soil Type</Form.Label>
-                                    <DropdownButton variant="dark" title="Preset Soil Types">
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.sand)}> Sand </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.loamySand)}> Loamy Sand </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.sandyLoam)}> Sandy Loam </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.loam)}> Loam </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.silt)}> Silt </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.siltLoam)}> Silt Loam </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.sandyClayLoam)}> Sandy Clay Loam </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.clayLoam)}> Clay Loam </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.siltyClayLoam)}> Silty Clay Loam </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.sandyClay)}> Sandy Clay </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.siltyClay)}> Silty Clay </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.clay)}> Clay </Dropdown.Item>
-                                        <Dropdown.Item onClick={() => setSoilPreset(soilTypes.undefined)}> Unknown </Dropdown.Item>
-                                    </DropdownButton>
-                                    <div className="pt-2" />
-                                    <Form.Label>N/H0</Form.Label>
-                                    <Form.Control
-                                        required
-                                        id="nh0"
-                                        type="number"
-                                        step="any"
-                                        size="lg"
-                                        min="0"
-                                        defaultValue={curInfiltrometerData.soilType.nh0}
-                                        placeholder="N/H0"
-                                    />
-                                    <div className="pt-2" />
-                                    <Form.Label>Alpha</Form.Label>
-                                    <Form.Control
-                                        required
-                                        id="alpha"
-                                        type="number"
-                                        step="any"
-                                        size="lg"
-                                        min="0"
-                                        defaultValue={curInfiltrometerData.soilType.alpha}
-                                        placeholder="Alpha"
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        Required!
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label className="pt-3 display-6">Radius (cm)</Form.Label>
-                                    <DropdownButton variant="dark" title="Preset Infiltrometer Types">
-                                        <Dropdown.Item onSelect={() => setRadiusPreset(infiltrometerTypes.MiniDisk.radius)}>
-                                            {infiltrometerTypes.MiniDisk.displayName}
-                                        </Dropdown.Item>
-                                        <Dropdown.Item onSelect={() => setRadiusPreset(infiltrometerTypes.MiniDiskV1.radius)}>
-                                            {infiltrometerTypes.MiniDiskV1.displayName}
-                                        </Dropdown.Item>
-                                    </DropdownButton>
-                                    <div className="pt-2" />
-                                    <Form.Control
-                                        required
-                                        id="radius"
-                                        type="number"
-                                        step="any"
-                                        size="lg"
-                                        min="0"
-                                        defaultValue={curInfiltrometerData.infiltrometerRadius}
-                                        placeholder="Radius (cm)"
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        Required!
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row className="mt-4">
-                            <Col>
-                                <Button type="submit" variant="dark" size="lg" className="w-100">Graph and Calculate Conductivity</Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </div>
+            <Container className="p-3 rounded border shadow">
+                <Form noValidate validated onSubmit={handleSubmit}>
+                    <Row>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label className="display-6">Soil Type</Form.Label>
+                                <DropdownButton variant="dark" title="Preset Soil Types">
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.sand)}> Sand </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.loamySand)}> Loamy Sand </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.sandyLoam)}> Sandy Loam </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.loam)}> Loam </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.silt)}> Silt </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.siltLoam)}> Silt Loam </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.sandyClayLoam)}> Sandy Clay Loam </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.clayLoam)}> Clay Loam </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.siltyClayLoam)}> Silty Clay Loam </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.sandyClay)}> Sandy Clay </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.siltyClay)}> Silty Clay </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.clay)}> Clay </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setSoilPreset(soilTypes.undefined)}> Unknown </Dropdown.Item>
+                                </DropdownButton>
+                                <div className="pt-2" />
+                                <Form.Label>N/H0</Form.Label>
+                                <Form.Control
+                                    required
+                                    id="nh0"
+                                    type="number"
+                                    step="any"
+                                    size="lg"
+                                    min="0"
+                                    defaultValue={curInfiltrometerData.soilType.nh0}
+                                    placeholder="N/H0"
+                                />
+                                <div className="pt-2" />
+                                <Form.Label>Alpha</Form.Label>
+                                <Form.Control
+                                    required
+                                    id="alpha"
+                                    type="number"
+                                    step="any"
+                                    size="lg"
+                                    min="0"
+                                    defaultValue={curInfiltrometerData.soilType.alpha}
+                                    placeholder="Alpha"
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Required!
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label className="pt-3 display-6">Radius (cm)</Form.Label>
+                                <DropdownButton variant="dark" title="Preset Infiltrometer Types">
+                                    <Dropdown.Item onClick={() => setRadiusPreset(infiltrometerTypes.MiniDisk.radius)}>
+                                        {infiltrometerTypes.MiniDisk.displayName}
+                                    </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => setRadiusPreset(infiltrometerTypes.MiniDiskV1.radius)}>
+                                        {infiltrometerTypes.MiniDiskV1.displayName}
+                                    </Dropdown.Item>
+                                </DropdownButton>
+                                <div className="pt-2" />
+                                <Form.Control
+                                    required
+                                    id="radius"
+                                    type="number"
+                                    step="any"
+                                    size="lg"
+                                    min="0"
+                                    defaultValue={curInfiltrometerData.infiltrometerRadius}
+                                    placeholder="Radius (cm)"
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Required!
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row className="mt-4">
+                        <Col>
+                            <Button type="submit" variant="dark" size="lg" className="w-100">Graph and Calculate Conductivity</Button>
+                        </Col>
+                    </Row>
+                </Form>
             </Container>
-        </>);
+        </>
+    );
 }
 
 
